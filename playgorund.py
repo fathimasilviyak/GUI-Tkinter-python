b0dd65605b4d9af549b5a97fdb3f5c018a7b23d6
fathimasilviyak <silviyakvm@gmail.com>
2023-04-04 15:33:12 +0530

    function with many keyworded arguments

--- playgorund.py
+++ playgorund.py
@@ -13,5 +13,18 @@ def add(*args):
     return total
 
 
-print(add(1, 2, 3))
-print(add(1, 4))
+# print(add(1, 2, 3))
+# print(add(1, 4))
+
+def calculate(n, **kwargs):
+    # print(kwargs)
+    # for (key,value) in kwargs.items():
+    #     print(key)
+    #     print(value)
+    # print(kwargs["add"])
+    # print(kwargs["multiply"])
+    n += kwargs['add']
+    n *= kwargs['multiply']
+    print(n)
+
+calculate(2, add=3, multiply=4)
