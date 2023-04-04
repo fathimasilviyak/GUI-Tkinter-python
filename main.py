4d90bc3aa59904590ff06afaf3ed28c5f5ebf64d
fathimasilviyak <silviyakvm@gmail.com>
2023-04-04 14:52:59 +0530

    functions with default arguments

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-import tkinter
-window = tkinter.Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
-my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
-my_label.pack(side ="left")
+# import tkinter
+# window = tkinter.Tk()
+# window.title("My First GUI Program")
+# window.minsize(width=500, height=300)
+# my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
+# my_label.pack(side ="left")
+#
+#
+#
+#
+# window.mainloop()
 
+def my_function(a=1,b=2, c=3):
+    print(a, b, c)
 
-
-
-window.mainloop()
+my_function()
+my_function(4, 5, 6)
+my_function(a=7, c=8)
