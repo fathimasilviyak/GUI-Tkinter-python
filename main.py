58bfd0b7f7c3c21c4caca561d3e826dabc0888ea
fathimasilviyak <silviyakvm@gmail.com>
2023-04-04 14:55:51 +0530

    functions with many arguments

--- main.py
+++ main.py
@@ -13,6 +13,14 @@
 def my_function(a=1,b=2, c=3):
     print(a, b, c)
 
-my_function()
-my_function(4, 5, 6)
-my_function(a=7, c=8)
+# my_function()
+# my_function(4, 5, 6)
+# my_function(a=7, c=8)
+
+def add(*args):
+    sum = 0
+    for n in args:
+        sum += n
+    print(sum)
+add(1,2,3)
+add(1,4)
