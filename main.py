514c7612d61210b2bb0d32885bb00f39e1663d9f
fathimasilviyak <silviyakvm@gmail.com>
2023-04-06 12:20:42 +0530

    change label text when button is clicked

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 # Label
 my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+my_label.pack()
 # my_label['text'] = "New Text"
-my_label.config(text="New text")
+# my_label.config(text="New text")
 
 
 # Button
 def button_clicked():
-    print("I got clicked")
+    my_label.config(text="Button got clicked")
 
 
 button = Button(text="Click me", command=button_clicked)
