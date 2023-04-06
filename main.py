dd24391ef65d3882a783f41c5d25176d77fc9a8e
fathimasilviyak <silviyakvm@gmail.com>
2023-04-06 13:31:33 +0530

    scale

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ window.minsize(width=500, height=300)
 # Label
 my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
+
+
 # my_label['text'] = "New Text"
 # my_label.config(text="New text")
 
@@ -36,11 +38,22 @@ text.insert(END, "Some text for multi-line text entry")
 print(text.get("1.0", END))
 text.pack()
 
+
 # Spinbox
 def spinbox_clicked():
     print(spinbox.get())
+
+
 spinbox = Spinbox(from_=0, to=10, width=10, command=spinbox_clicked)
 spinbox.pack()
 
+
+# Scale
+def scale_used(value):
+    print(value)
+
+
+scale = Scale(from_=0, to=100, command=scale_used)
+scale.pack()
 
 window.mainloop()
