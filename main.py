f6754b0f21967ffb310519912d8f3004766e5a6e
fathimasilviyak <silviyakvm@gmail.com>
2023-04-06 13:10:49 +0530

    text box for multiline entry

--- main.py
+++ main.py
@@ -13,14 +13,28 @@ my_label.pack()
 # Button
 def button_clicked():
     # my_label.config(text="Button got clicked")
-    my_label.config(text=input_name.get())
+    new_text = input_name.get()
+    my_label.config(text=new_text)
 
 
 button = Button(text="Click me", command=button_clicked)
 button.pack()
 
 # Entry
-input_name = Entry(width=10)
+input_name = Entry(width=30)
+# Add some text to begin with
+input_name.insert(END, string="Some text to begin with")
 input_name.pack()
+
+# Text
+text = Text(height=5, width=30)
+# Put cursor in text box
+text.focus()
+# Add some text to begin with
+text.insert(END, "Some text for multi-line text entry")
+# get hold of the text inside the text box starting from first line and zeroth character
+print(text.get("1.0", END))
+text.pack()
+
 
 window.mainloop()
