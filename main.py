b07cae71471b058d54a9f220a9772203cb56fcaf
fathimasilviyak <silviyakvm@gmail.com>
2023-04-06 12:17:05 +0530

    button with even listener

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-# import tkinter
-# window = tkinter.Tk()
-# window.title("My First GUI Program")
-# window.minsize(width=500, height=300)
-# my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "bold"))
-# my_label.pack(side ="left")
-#
-#
-#
-#
-# window.mainloop()
+from tkinter import *
+
+window = Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+# Label
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label.pack(side="left")
+# my_label['text'] = "New Text"
+my_label.config(text="New text")
 
 
+# Button
+def button_clicked():
+    print("I got clicked")
+
+
+button = Button(text="Click me", command=button_clicked)
+button.pack()
+
+window.mainloop()
